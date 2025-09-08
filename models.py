5e2b27b74e02d131a0e0f3550febeb7a78d3de0a
Sagi Shnaidman <sshnaidm@redhat.com>
2025-09-09 00:34:53 +0300

    Add choosing model from env vars

--- models.py
+++ models.py
@@ -4,12 +4,30 @@ import os
 
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_ollama import ChatOllama
+from langchain_openai import ChatOpenAI
 
 gemini = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash", temperature=0.0, top_p=0.0, google_api_key=os.getenv("GEMINI_API_KEY")
 )
 
+openai = ChatOpenAI(model="gpt-4o-mini", temperature=0.0, top_p=0.0, api_key=os.getenv("OPENAI_API_KEY"))
 
 # ollama based model
-ollama = ChatOllama(model="qwen3:8b", temperature=0.0, top_p=0.0, num_ctx=40000)
+ollama = lambda ol_model: ChatOllama(model=ol_model, temperature=0.0, top_p=0.0, num_ctx=40000)
 #ollama = ChatOllama(model="qwen2.5:7b", temperature=0.0, top_p=0.0, num_ctx=40000)
+
+def init_llm() -> ChatOpenAI | ChatOllama | ChatGoogleGenerativeAI:
+    print("For choosing the model, set the MODEL environment variable to one of: openai, ollama/<model_name>, gemini")
+    print("MODEL environment variable:", os.getenv("MODEL"))
+    if os.getenv("MODEL"):
+        if os.getenv("MODEL") == "openai":
+            llm = openai
+        elif os.getenv("MODEL").startswith("ollama"):
+            llm = ollama(os.getenv("MODEL").replace("ollama/", ""))
+        elif os.getenv("MODEL") == "gemini":
+            llm = gemini
+    else:
+        llm = gemini
+
+    print("LLM initialized:", llm)
+    return llm
